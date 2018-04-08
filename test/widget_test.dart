884cbbfb769c46ac9b6117044ce7db52a62931f8
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-04-08 04:31:39 -0300

    Replace indent with 4-spaces by 2-spaces.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:startup_namer/main.dart';
 
 void main() {
-    testWidgets('App starts with Hello World presented', (WidgetTester tester) async {
-        // Build our app and trigger a frame.
-        await tester.pumpWidget(new StartupNamer());
+  testWidgets('App starts with Hello World presented', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(new StartupNamer());
 
-        // Verify that our text is not 'Hello World'.
-        expect(find.text('Hello World'), findsNothing);
+    // Verify that our text is not 'Hello World'.
+    expect(find.text('Hello World'), findsNothing);
   });
 }
