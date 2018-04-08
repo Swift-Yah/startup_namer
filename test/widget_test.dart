3126a77c31b230f7b642ae56fc40ffcb4e79545a
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-04-08 02:50:48 -0300

    Replace all code by a real "Hello World" sample.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,26 +4,15 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:startup_namer/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    testWidgets('App starts with Hello World presented', (WidgetTester tester) async {
+        // Build our app and trigger a frame.
+        await tester.pumpWidget(new StartupNamer());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+        // Verify that our text is 'Hello World'.
+        expect(find.text('Hello World'), findsOneWidget);
   });
 }
