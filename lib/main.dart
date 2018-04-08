167bb2f66bf68983ce56afc806b00549b1cd2da5
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-04-08 04:02:39 -0300

    Make list be interactive and each word can be favorited.

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomStateWords extends State<RandomWords> {
     final _suggestions = <WordPair>[];
+    final _saved = new Set<WordPair>();
     final _biggerFont = const TextStyle(fontSize: 18.0);
 
     @override
@@ -50,11 +51,26 @@ class RandomStateWords extends State<RandomWords> {
     }
 
     Widget _buildRow(WordPair pair) {
+        final alreadySaved = _saved.contains(pair);
+
         return new ListTile(
             title: new Text(
                 pair.asPascalCase,
                 style: _biggerFont,
             ),
+            trailing: new Icon(
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved ? Colors.red : null,
+            ),
+            onTap: () {
+                setState(() {
+                    if (alreadySaved) {
+                        _saved.remove(pair);
+                    } else {
+                        _saved.add(pair);
+                    }
+                });
+            },
         );
     }
 }
