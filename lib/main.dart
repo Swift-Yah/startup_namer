f06b0bd85390819c51f7877c0ccae696e0262e6d
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-04-08 03:17:33 -0300

    Make words generation separate into a `StatefulWidget`.

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ void main() => runApp(new StartupNamer());
 class StartupNamer extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        final wordPair = new WordPair.random();
-
         return new MaterialApp(
             title: 'Welcome to Flutter',
             home: new Scaffold(
@@ -15,9 +13,23 @@ class StartupNamer extends StatelessWidget {
                     title: new Text('Welcome to Flutter'),
                 ),
                 body: new Center(
-                    child: new Text(wordPair.asPascalCase),
+                    child: new RandomWords(),
                 ),
             ),
         );
+    }
+}
+
+class RandomWords extends StatefulWidget {
+    @override
+    createState() => new RandomStateWords();
+}
+
+class RandomStateWords extends State<RandomWords> {
+    @override
+    Widget build(BuildContext context) {
+        final wordPair = new WordPair.random();
+
+        return new Text(wordPair.asPascalCase);
     }
 }
