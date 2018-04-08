954010e6088a9bd3980e52dd8fb1a121c4d24d26
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-04-08 04:29:00 -0300

    Change primary color to `Colors.white`.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class StartupNamer extends StatelessWidget {
     Widget build(BuildContext context) {
         return new MaterialApp(
             title: 'Startup Name Generator',
+            theme: new ThemeData(
+                primaryColor: Colors.white,
+            ),
             home: new RandomWords(),
         );
     }
