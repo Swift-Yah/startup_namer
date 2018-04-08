8cb054dfa4c74e3be3edaf2063051c0dd1773ac8
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2018-04-08 04:23:52 -0300

    Make usage of route to push and pop to other page.

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,9 @@ class RandomStateWords extends State<RandomWords> {
         return new Scaffold(
             appBar: new AppBar(
                 title: new Text('Startup Name Generator'),
+                actions: <Widget>[
+                    new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+                ],
             ),
             body: _buildSuggestions(),
         );
@@ -71,6 +74,39 @@ class RandomStateWords extends State<RandomWords> {
                     }
                 });
             },
+        );
+    }
+
+    void _pushSaved() {
+        Navigator.of(context).push(
+            new MaterialPageRoute(
+                builder: (context) {
+                    final tiles = _saved.map(
+                            (pair) {
+                            return new ListTile(
+                                title: new Text(
+                                    pair.asPascalCase,
+                                    style: _biggerFont,
+                                ),
+                            );
+                        }
+                    );
+
+                    final divided = ListTile
+                        .divideTiles(
+                        context: context,
+                        tiles: tiles
+                    )
+                        .toList();
+
+                    return new Scaffold(
+                        appBar: new AppBar(
+                            title: new Text('Saved Suggestions'),
+                        ),
+                        body: new ListView(children: divided)
+                    );
+                }
+            )
         );
     }
 }
